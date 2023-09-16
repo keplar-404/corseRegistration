252c050c010d82fe89a2a81c1005ae35365c05a1
keplar-404 <shehabhossen76.services@gmail.com>
2023-09-16 23:13:42 +0600

    done5

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("http://localhost:3030/data");
+      const response = await fetch("https://course-registration-ozq3.onrender.com:3030/data");
       const data = await response.json();
       setFetchData(data);
     };
